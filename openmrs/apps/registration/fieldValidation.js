c8740e619e6a29e59d5835bc2052a4cfe554a5e4
MD Dubey <mdinfotech007@gmail.com>
2015-09-02 18:46:19 +0530

    Dubey,Sukreet | BDSHR-908 | Added js validation for HealthId.

---
+++ openmrs/apps/registration/fieldValidation.js
@@ -0,0 +1,44 @@
+var getLunhAlgorithmSum = function(healthId) {
+	var lunhAlgorithmSum = 0,
+		currentDigit = 0,
+		isEvenIndexFromLast = false;
+
+	for (var n = healthId.length - 1; n >= 1; n--) {
+		var cDigit = healthId.charAt(n),
+			currentDigit = parseInt(cDigit, 10);
+
+		if (isEvenIndexFromLast) {
+			if ((currentDigit *= 2) > 9) currentDigit -= 9;
+		}
+
+		lunhAlgorithmSum += currentDigit;
+		isEvenIndexFromLast = !isEvenIndexFromLast;
+	}
+	return lunhAlgorithmSum;
+}
+
+var containFourConsecutiveDigits = function(healthId) {
+	var healthIdToCheck = healthId.slice(0, healthId.length - 1);
+	return healthIdToCheck.match(/([0-9])\1\1\1/);
+}
+
+var containTwoSetsOfThreeConsecutiveDigits = function(healthId) {
+	var healthIdToCheck = healthId.slice(0, healthId.length - 1);
+	var matches = healthIdToCheck.match(/([0-9])\1\1/g);
+	return matches && matches.length > 1;
+}
+
+Bahmni.Registration.customValidator = {
+	"Health ID": {
+		method: function(name, value, attributeDetails) {
+			if (containFourConsecutiveDigits(value)) {
+				return false;
+			}
+			if (containTwoSetsOfThreeConsecutiveDigits(value)) {
+				return false
+			}
+			return (getLunhAlgorithmSum(value) % 10) == 0;
+		},
+		errorMessage: "Please Enter Valid HID."
+	}
+};
