6ecfcf323f426d28c822f83fd8426220592c0f53
sadiag <sadiag@INsadiag.local>
2015-10-06 18:57:07 +0530

    new configuration changes for custom display control

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -30,4 +30,40 @@ angular.module('bahmni.common.displaycontrol.custom')
             link: link,
             template: '<ng-include src="contentUrl"/>'
         }
+    }]).directive('dischargeSummary', ['$q','visitService', 'bedService','appService', 'spinner', function ($q, visitService, bedService,appService, spinner) 
+    {
+        var link = function ($scope) 
+        {
+            var conceptNames = ["BMI"];
+            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/dischargeSummary.html";
+            
+            spinner.forPromise($q.all([bedService.getAssignedBedForPatient($scope.patient.uuid),visitService.getVisitSummary($scope.visitUuid)]).then(function(results){
+                    $scope.bedDetails = results[0];
+                    $scope.visitSummary = results[1].data;
+                }));
+                
+        };
+
+        return {
+            restrict: 'E',
+            link: link,
+            template: '<ng-include src="contentUrl"/>',
+        }
+    }]).directive('footerSummary', ['visitService','appService', 'spinner', function (visitService,appService, spinner) 
+    {
+       var link = function ($scope) 
+        {
+            var conceptNames = ["BMI"];
+            
+            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/footerSummary.html";
+                spinner.forPromise(visitService.getVisitSummary($scope.visitUuid).then(function(results){
+					$scope.visitSummary = results.data;
+                }));
+        };
+
+        return {
+            restrict: 'E',
+            link: link,
+            template: '<ng-include src="contentUrl"/>',
+        }
     }]);
