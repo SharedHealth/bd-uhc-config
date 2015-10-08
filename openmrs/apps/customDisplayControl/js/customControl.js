11af2fc9bd99abe022714c271cfe5b96e9b74525
arunpaul <arunpaul@INarunpaul1.local>
2015-10-08 09:51:01 +0530

    New configuration changes, printouts

--- openmrs/apps/customDisplayControl/js/customControl.js
+++ openmrs/apps/customDisplayControl/js/customControl.js
@@ -16,19 +16,38 @@ angular.module('bahmni.common.displaycontrol.custom')
                 template: '<ng-include src="contentUrl"/>',
                 link: link
             }
-    }]).directive('deathCertificate', ['observationsService', 'appService', 'spinner', function (observationsService, appService, spinner) {
-        var link = function ($scope) {
-            var conceptNames = ["WEIGHT"];
+    }]).directive('deathCertificate', ['$q','visitService', 'bedService','appService', 'spinner', function ($q, visitService, bedService,appService, spinner) 
+    {
+        var link = function ($scope) 
+        {
+            var conceptNames = ["BMI"];
             $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/deathCertificate.html";
-            spinner.forPromise(observationsService.fetch($scope.patient.uuid, conceptNames, "latest", undefined, $scope.visitUuid, undefined).then(function (response) {
-                $scope.observations = response.data;
-            }));
+            
+            spinner.forPromise($q.all([bedService.getAssignedBedForPatient($scope.patient.uuid),visitService.getVisitSummary($scope.visitUuid)]).then(function(results){
+                    $scope.bedDetails = results[0];
+                    $scope.visitSummary = results[1].data;
+                }));
+                
+        };
+        return {
+            restrict: 'E',
+            link: link,
+            template: '<ng-include src="contentUrl"/>'
+        }
+    }]).directive('deathFooter', ['visitService','appService', 'spinner', function (visitService,appService, spinner) 
+    {
+       var link = function ($scope) 
+        {
+            var conceptNames = ["BMI"];
+            
+            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/deathFooter.html";
+            $scope.curDate=new Date();
         };
 
         return {
             restrict: 'E',
             link: link,
-            template: '<ng-include src="contentUrl"/>'
+            template: '<ng-include src="contentUrl"/>',
         }
     }]).directive('dischargeSummary', ['$q','visitService', 'bedService','appService', 'spinner', function ($q, visitService, bedService,appService, spinner) 
     {
@@ -59,6 +78,21 @@ angular.module('bahmni.common.displaycontrol.custom')
                 spinner.forPromise(visitService.getVisitSummary($scope.visitUuid).then(function(results){
 					$scope.visitSummary = results.data;
                 }));
+        };
+
+        return {
+            restrict: 'E',
+            link: link,
+            template: '<ng-include src="contentUrl"/>',
+        }
+    }]).directive('prescriptionFooter', ['visitService','appService', 'spinner', function (visitService,appService, spinner) 
+    {
+       var link = function ($scope) 
+        {
+            var conceptNames = ["BMI"];
+            
+            $scope.contentUrl = appService.configBaseUrl() + "/customDisplayControl/views/prescription.html";
+            $scope.curDate=new Date();
         };
 
         return {
