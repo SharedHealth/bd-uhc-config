eac1dcbe8644f1020ed615987265a7c1ff2fb7aa
arunpaul <paul.arun@gmail.com>
2016-03-16 08:07:53 +0600

    changes to BOR script

--- openmrs/apps/reports/sql/bor.sql
+++ openmrs/apps/reports/sql/bor.sql
@@ -1,29 +1,32 @@
-Select (select sum(datediff(e2.date_created,e1.date_created))
+Select
+        (select count(*) from bed where bed_type_id=1) as 'No. of beds',
+
+        (datediff('#endDate#','#startDate#')) as 'No. of days',
+
+        (select sum(datediff(e2.date_created,e1.date_created))
         from patient p JOIN visit v on p.patient_id=v.patient_id
           join encounter e1 on v.visit_id = e1.visit_id
           join encounter_type et on et.encounter_type_id = e1.encounter_type
           JOIN encounter e2 on v.visit_id = e2.visit_id
           join encounter_type et2  on et2.encounter_type_id = e2.encounter_type
-        where et.name='ADMISSION' and et2.name='DISCHARGE'and
-              date(e1.date_created) between '#startDate#'and '#endDate#'
-              and date(e2.date_created) between '#startDate#' and '#endDate#' ) as sumofdatediff,
+        where et.name='ADMISSION' and et2.name='DISCHARGE'
+              and date(e2.date_created) between '#startDate#' and '#endDate#' ) as 'Total LOS',
+
        (select count(p.patient_id)
         from patient p JOIN visit v on p.patient_id=v.patient_id
           join encounter e1 on v.visit_id = e1.visit_id
           join encounter_type et on et.encounter_type_id = e1.encounter_type
           JOIN encounter e2 on v.visit_id = e2.visit_id
           join encounter_type et2  on et2.encounter_type_id = e2.encounter_type
-        where et.name='ADMISSION' and et2.name='DISCHARGE'and
-              date(e1.date_created) between '#startDate#'and '#endDate#'
-              and date(e2.date_created) between '#startDate#' and '#endDate#') noofpatient,
-       (select sum(datediff(e2.date_created,e1.date_created))
+        where et.name='ADMISSION' and et2.name='DISCHARGE'
+              and date(e2.date_created) between '#startDate#' and '#endDate#') as 'No. of patients discharged',
+       ROUND((select sum(datediff(e2.date_created,e1.date_created))
         from patient p JOIN visit v on p.patient_id=v.patient_id
           join encounter e1 on v.visit_id = e1.visit_id
           join encounter_type et on et.encounter_type_id = e1.encounter_type
           JOIN encounter e2 on v.visit_id = e2.visit_id
           join encounter_type et2  on et2.encounter_type_id = e2.encounter_type
-        where et.name='ADMISSION' and et2.name='DISCHARGE'and
-              date(e1.date_created) between '#startDate#'and '#endDate#'
+        where et.name='ADMISSION' and et2.name='DISCHARGE'
               and date(e2.date_created) between '#startDate#' and '#endDate#')/
        (select count(p.patient_id)
         from patient p JOIN visit v on p.patient_id=v.patient_id
@@ -31,10 +34,9 @@ Select (select sum(datediff(e2.date_created,e1.date_created))
           join encounter_type et on et.encounter_type_id = e1.encounter_type
           JOIN encounter e2 on v.visit_id = e2.visit_id
           join encounter_type et2  on et2.encounter_type_id = e2.encounter_type
-        where et.name='ADMISSION' and et2.name='DISCHARGE'and
-              date(e1.date_created) between '#startDate#'and '#endDate#'
-              and date(e2.date_created) between '#startDate#' and '#endDate#') as ALS,
-  (
+        where et.name='ADMISSION' and et2.name='DISCHARGE'
+              and date(e2.date_created) between '#startDate#' and '#endDate#'),2) as ALOS,
+        ROUND((
          ((select sum(datediff(e2.date_created,e1.date_created))
            from patient p JOIN visit v on p.patient_id=v.patient_id
              join encounter e1 on v.visit_id = e1.visit_id
@@ -42,8 +44,7 @@ Select (select sum(datediff(e2.date_created,e1.date_created))
              JOIN encounter e2 on v.visit_id = e2.visit_id
              join encounter_type et2  on et2.encounter_type_id = e2.encounter_type
            where et.name='ADMISSION' and et2.name='DISCHARGE'and
-                 date(e1.date_created) between '#startDate#'and '#endDate#'
-                 and date(e2.date_created) between '#startDate#' and '#endDate#')/
+                 date(e1.date_created) between '#startDate#'and '#endDate#')/
           (select count(p.patient_id)
            from patient p JOIN visit v on p.patient_id=v.patient_id
              join encounter e1 on v.visit_id = e1.visit_id
@@ -51,15 +52,6 @@ Select (select sum(datediff(e2.date_created,e1.date_created))
              JOIN encounter e2 on v.visit_id = e2.visit_id
              join encounter_type et2  on et2.encounter_type_id = e2.encounter_type
            where et.name='ADMISSION' and et2.name='DISCHARGE'and
-                 date(e1.date_created) between '#startDate#'and '#endDate#'
-                 and date(e2.date_created) between '#startDate#' and '#endDate#')*100)/
-         (
-           (Select (select count(*) from bed )*(datediff('#endDate#','#startDate#'))
-           )
-         )
-       )as BOR,
-  (select count(*) from bed) as Numofbeds,
-  (datediff('#endDate#','#startDate#')) as datediff
-
-
-;
+                 date(e1.date_created) between '#startDate#'and '#endDate#')*100)/
+         ((Select (select count(*) from bed where bed_type_id=1 )*(datediff('#endDate#','#startDate#'))))
+       ),2)as BOR;
