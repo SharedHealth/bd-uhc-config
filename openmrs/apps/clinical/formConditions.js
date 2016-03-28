3346faf79d999339c4203a324d9d7a0d763e1327
sadiag <sadiag@INsadiag.local>
2016-03-28 12:14:17 +0530

    #481 sadia | added enable and disable feature for IMCI template

--- openmrs/apps/clinical/formConditions.js
+++ openmrs/apps/clinical/formConditions.js
@@ -25,4 +25,31 @@ Bahmni.ConceptSet.FormConditions.rules = {
             }
         }
     }
+};Bahmni.ConceptSet.FormConditions.rules = {
+    'Diastolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic'];
+        var diastolic = formFieldValues['Diastolic'];
+        if (systolic || diastolic) {
+            return {
+                enable: ["Posture"]
+            }
+        } else {
+            return {
+                disable: ["Posture"]
+            }
+        }
+    },
+    'Systolic Data' : function (formName, formFieldValues) {
+        var systolic = formFieldValues['Systolic'];
+        var diastolic = formFieldValues['Diastolic'];
+        if (systolic || diastolic) {
+            return {
+                enable: ["Posture"]
+            }
+        } else {
+            return {
+                disable: ["Posture"]
+            }
+        }
+    }
 };
