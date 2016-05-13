24037650e25845b9604c23fb0b8df64d2ccb9e6c
sadiag <sadiag@INsadiag.local>
2016-05-13 10:24:14 +0530

    added apostrophe

--- openmrs/apps/reports/sql/coded-noncoded-drugcount.sql
+++ openmrs/apps/reports/sql/coded-noncoded-drugcount.sql
@@ -1,11 +1,11 @@
 select user.username as UserName,
-  sum(if(drug_inventory_id is not null,1,0)) as Coded Drug Orders,
-sum(if(drug_inventory_id is null,1,0)) as Non Coded Drugs Orders
+       sum(if(drug_inventory_id is not null,1,0)) as 'Coded Drug Orders',
+sum(if(drug_inventory_id is null,1,0)) as 'Non Coded Drugs Orders'
 from drug_order dr inner join orders o on o.order_id=dr.order_id and cast(o.date_created as date) BETWEEN '#startDate#' AND '#endDate#'
 inner join provider pr on pr.provider_id=o.orderer
 inner join encounter e on o.encounter_id = e.encounter_id
-  INNER join location l on l.location_id = e.location_id
-  INNER JOIN location_tag_map lm on l.location_id= lm.location_id
-  inner JOIN location_tag lt on lt.location_tag_id = lm.location_tag_id and lt.name='Report Location'
-  INNER JOIN users  user ON user.person_id = pr.person_id
+INNER join location l on l.location_id = e.location_id
+INNER JOIN location_tag_map lm on l.location_id= lm.location_id
+inner JOIN location_tag lt on lt.location_tag_id = lm.location_tag_id and lt.name='Report Location'
+INNER JOIN users  user ON user.person_id = pr.person_id
 group by user.username;
--- openmrs/apps/reports/sql/ordertypebyprovider.sql
+++ openmrs/apps/reports/sql/ordertypebyprovider.sql
@@ -1,13 +1,11 @@
 
-
-
 SELECT
-  users.username      AS Provider,
-  ot.name             AS Order Type,
-  count(ord.order_id) AS Count of orders
+       users.username      AS Provider,
+       ot.name             AS 'Order Type',
+count(ord.order_id) AS 'Count of orders'
 FROM orders ord INNER JOIN order_type ot
-    ON ot.order_type_id = ord.order_type_id and cast(ord.date_created as DATE) between '#startDate#' and '#endDate#'
-  INNER JOIN provider p ON ord.orderer = p.provider_id
-  INNER JOIN users ON users.person_id = p.person_id
-GROUP BY  Provider,Order_type
+ON ot.order_type_id = ord.order_type_id and cast(ord.date_created as DATE) between '#startDate#' and '#endDate#'
+INNER JOIN provider p ON ord.orderer = p.provider_id
+INNER JOIN users ON users.person_id = p.person_id
+GROUP BY  Provider,'Order Type'
 ORDER BY Provider;
