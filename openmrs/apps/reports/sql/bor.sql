748e869aefca8cca553645a39b991e50b860e44d
MD Dubey <mddubey409@gmail.com>
2017-02-23 13:14:26 +0600

    Dubey | SHR-000 | Changed SQLs for Bed Statement (DHIS2). Queries were eliminating one day.

--- openmrs/apps/reports/sql/bor.sql
+++ openmrs/apps/reports/sql/bor.sql
@@ -1,9 +1,9 @@
 Select
-        (select count(*) from bed where bed_type_id=1) as 'No. of beds',
+        (select count(*) from bed where bed_type_id=1 or bed_type_id = 3 as 'No. of beds',
 
-        (datediff('#endDate#','#startDate#')) as 'No. of days',
+        (datediff('#endDate#','#startDate#')+1) as 'No. of days',
 
-        (select sum(datediff(e2.date_created,e1.date_created))
+        (select sum(datediff(e2.date_created,e1.date_created)+1)
         from patient p JOIN visit v on p.patient_id=v.patient_id
           join encounter e1 on v.visit_id = e1.visit_id
           join encounter_type et on et.encounter_type_id = e1.encounter_type
@@ -20,7 +20,7 @@ Select
           join encounter_type et2  on et2.encounter_type_id = e2.encounter_type
         where et.name='ADMISSION' and et2.name='DISCHARGE'
               and date(e2.date_created) between '#startDate#' and '#endDate#') as 'No. of patients discharged',
-       ROUND((select sum(datediff(e2.date_created,e1.date_created))
+       ROUND((select sum(datediff(e2.date_created,e1.date_created)+1)
         from patient p JOIN visit v on p.patient_id=v.patient_id
           join encounter e1 on v.visit_id = e1.visit_id
           join encounter_type et on et.encounter_type_id = e1.encounter_type
@@ -37,7 +37,7 @@ Select
         where et.name='ADMISSION' and et2.name='DISCHARGE'
               and date(e2.date_created) between '#startDate#' and '#endDate#'),2) as ALOS,
         ROUND((
-         ((select sum(datediff(e2.date_created,e1.date_created))
+         ((select sum(datediff(e2.date_created,e1.date_created)+1)
            from patient p JOIN visit v on p.patient_id=v.patient_id
              join encounter e1 on v.visit_id = e1.visit_id
              join encounter_type et on et.encounter_type_id = e1.encounter_type
@@ -53,5 +53,5 @@ Select
              join encounter_type et2  on et2.encounter_type_id = e2.encounter_type
            where et.name='ADMISSION' and et2.name='DISCHARGE'and
                  date(e1.date_created) between '#startDate#'and '#endDate#')*100)/
-         ((Select (select count(*) from bed where bed_type_id=1 )*(datediff('#endDate#','#startDate#'))))
+         ((Select (select count(*) from bed where bed_type_id=1 or bed_type_id = 3)*(datediff('#endDate#','#startDate#')+1)))
        ),2)as BOR;
